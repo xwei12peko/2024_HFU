2d98626b06cef554a70aff61f85a47606a096b88
xwei12peko <kgc2924r@gmail.com>
2024-08-13 11:57:33 +0800

    [feature] ask weather with chatgpt response

--- openai_api.py
+++ openai_api.py
@@ -3,24 +3,9 @@ import os
 from pprint import pprint
 
 
-# {
-#     'user_id_a': [
-#         {'role': 'user', 'content': 'xxx'},
-#         {'role': 'system', 'content': 'yyy'},
-#         {'role': 'user', 'content': 'xxx'},
-#         {'role': 'system', 'content': 'yyy'},
-#     ],
-#     'user_id_b': [
-#         {'role': 'user', 'content': 'aaa'},
-#         {'role': 'system', 'content': 'bbb'},
-#         {'role': 'user', 'content': 'ccc'},
-#         {'role': 'system', 'content': 'ddd'},
-#     ]
-# }
-
 chat_history = dict()
 
-def chat_with_chatgpt(user_id, user_message, openai_api_key):
+def chat_with_chatgpt(user_id, user_message, openai_api_key, extra_prompt=""):
     # 利用OpenAI類別，建立一個可以跟OpenAI伺服器互動的物件
     client = OpenAI(api_key=openai_api_key)
 
@@ -29,7 +14,7 @@ def chat_with_chatgpt(user_id, user_message, openai_api_key):
         chat_history[user_id].append({"role": "user", "content": user_message})
     else:
         chat_history[user_id] = [{"role": "user", "content": user_message}]
-    message = user_message + '  請用條列式的方式回答。' # 加料
+    message = user_message + extra_prompt # 加料
     
     # 完成一段對話
     chat_completion = client.chat.completions.create(
