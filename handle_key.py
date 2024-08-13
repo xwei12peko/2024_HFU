20d06fe3c49e2f4256a468e917b22c4f18cb162c
xwei12peko <kgc2924r@gmail.com>
2024-08-13 11:00:35 +0800

    [feature & refactor] cwa scraper with linebot.

--- handle_key.py
+++ handle_key.py
@@ -1,26 +1,24 @@
 import os, sys
 
 def get_secret_and_token():
-    # https://github.com/line/line-bot-sdk-python/blob/master/examples/flask-echo/app_with_handler.py   #從此地拉的code
-    # 1.先到LINE Developer Console，把Channel Secret & Channel Accsss Token複製起來
-    # 2.把這兩個密文，存到環境變數內；工具列搜尋<環境變數>，新增兩個環境變數，並把值貼上去
-    # 3.按下確定儲存，要記得變數名稱，這些資訊只會存在你當前使用的電腦裡
-    # 4.透過以下程式碼，取得環境變數儲存的對應數值
-    channel_secret = os.getenv('LINEBOT_SECRET_KEY', None)
-    channel_access_token = os.getenv('LINEBOT_ACCESS_TOKEN', None)
-    openapi_key = os.getenv("OPENAI_API_KEY", None)
-    if channel_secret is None:
-        print('Specify LINEBOT_SECRET_KEY as environment variable.')
-        sys.exit(1)
-    if channel_access_token is None:
-        print('Specify LINEBOT_ACCESS_TOKEN as environment variable.')
-        sys.exit(1)
-    if openapi_key is None:
-        print('Specify OPENAI_API_KEY as environment variable.')
-        sys.exit(1)
+    # 1. 先到Line Developer Console，把 Channel Secret & Channel Access Token複製起來
+    # 2. 把這兩個密文，存到環境變數內；工具列搜尋<環境變數>，新增兩個環境變數，並且把值貼上去
+    # 3. 按下確定儲存，要記得你的變數名稱，這些資訊只會存在你當前使用的電腦裡。
+    # 4. 透過以下程式碼，取得環境變數儲存的對應數值。
 
-    return {
-        'LINEBOT_SECRET_KEY': channel_secret,
-        'LINEBOT_ACCESS_TOKEN': channel_access_token,
-        'OPENAI_API_KEY': openapi_key
-    } 
+    tokens_name = [
+        'LINEBOT_SECRET_KEY',
+        'LINEBOT_ACCESS_TOKEN',
+        "OPENAI_API_KEY",
+        "CWA_API_KEY"
+    ]
+
+    keys = dict()
+    for token_name in tokens_name:
+        token = os.getenv(token_name, None)
+        if token is None:
+            print(f'Specify {token_name} as environment variable.')
+            sys.exit(1)
+        keys[token_name] = token
+
+    return keys
